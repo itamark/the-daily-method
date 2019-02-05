26c5a62a7e41241687c3ba99d3c3d8f7fee74f37
Itamar Kestenbaum <itamar@next-insurance.com>
2019-02-05 13:59:57 +0200

    style fixes

--- src/method/Method.js
+++ src/method/Method.js
@@ -4,7 +4,7 @@ export default class Method extends Component {
     return (
       <div>
         <h1>{this.props.method.title}</h1>
-        <h3>{this.props.method.summary}</h3>
+        <p>{this.props.method.summary}</p>
       </div>
     )
   }
