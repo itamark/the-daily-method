bfb7c46e882703efd1aaf1620ac59d6eeacabf95
Itamar Kestenbaum <itamar@next-insurance.com>
2019-02-05 14:03:23 +0200

    Add build folder

---
+++ build/precache-manifest.86738da17a2eeb92dfae6b3e5ffafddf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e46950710e78cd844cfe",
+    "url": "/static/js/main.e4695071.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "e46950710e78cd844cfe",
+    "url": "/static/css/main.e481c6a2.chunk.css"
+  },
+  {
+    "revision": "8ff34acfae9c9691a60a734f0f6b6dd2",
+    "url": "/index.html"
+  }
+];
