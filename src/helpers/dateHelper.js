d1830537c6d61cf4983c6c6b64ead19c329fe53d
Ofer Vugman <vugman.ofer@gmail.com>
2019-02-10 10:22:50 +0200

    PR fixes

--- src/helpers/dateHelper.js
+++ src/helpers/dateHelper.js
@@ -1,17 +1,17 @@
 export function getDay(now = new Date()){
-    var start = new Date(now.getFullYear(), 0, 0);
-    var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
-    var oneDay = 1000 * 60 * 60 * 24;
-    var day = Math.floor(diff / oneDay);
+    const start = new Date(now.getFullYear(), 0, 0);
+    const diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+    const oneDay = 1000 * 60 * 60 * 24;
+    const day = Math.floor(diff / oneDay);
     return day;
 }
 
 export function incrementDate(date = new Date()){
-    return new Date(date.getTime() + 24 * 60 * 60 * 1000);
+    return new Date(date.setDate(date.getDate() + 1));
 }
 
 export function decrementDate(date = new Date()){
-    return new Date(date.getTime() - 24 * 60 * 60 * 1000);
+    return new Date(date.setDate(date.getDate() - 1));
 }
 
-export default getDay
+export default getDay;
